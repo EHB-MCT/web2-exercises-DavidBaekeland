193e33adaaec2d5e834b58af6d13fd34612382ca
David Baekeland <david.baekeland@student.ehb.be>
2021-10-04 16:36:28 +0200

    S1: object

--- S1/index.js
+++ S1/index.js
@@ -6,16 +6,28 @@ window.onload = function() {
     document.getElementById("form").addEventListener("submit", event => {
         event.preventDefault();
 
-        // get values
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value; 
-        const order = document.getElementById("order").value;  
-        console.log(name);
+        // // get values
+        // const name = document.getElementById("name").value;
+        // const email = document.getElementById("email").value; 
+        // const order = document.getElementById("order").value;  
+        // console.log(name);
 
-        // print on screen
-        const message = `The order for the customer ${name}  is the following:  ${order}. The customer may be notified by email:  ${email}`;
-        document.getElementById("container").innerHTML = message
+        // // print on screen
+        // const message = `The order for the customer ${name}  is the following:  ${order}. The customer may be notified by email:  ${email}`;
+        // document.getElementById("container").innerHTML = message
 
+        const order = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            order: document.getElementById("order").value,
+            printOrder(name, email, order) {
+                const message = `The order for the customer ${name}  is the following:  ${order}. The customer may be notified by email:  ${email}`;
+                document.getElementById("container").innerHTML = message
+            }
+        };
+
+
+        order.printOrder(order.name, order.email, order.order)
         
     });
 
