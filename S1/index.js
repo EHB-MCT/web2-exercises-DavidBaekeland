d2a8e9f04ba7cc036af4da64fb0a4a18f03d0257
David Baekeland <david.baekeland@student.ehb.be>
2021-10-04 19:54:05 +0200

    S1: 6: index: order, js: array, forEach, checked

--- S1/index.js
+++ S1/index.js
@@ -3,6 +3,32 @@
 window.onload = function() {
     console.log("test");
 
+
+    // array
+    const dishes = [];
+    dishes.push({
+        id: "1",
+        name: "pizza Mozarella",
+        price: "10"
+    });
+    dishes.push({
+        id: "2",
+        name: "tomato soep",
+        price: "3"
+    });
+    dishes.push({
+        id: "3",
+        name: "Pasta",
+        price: "5"
+    });
+
+    dishes.forEach(dish => {
+        console.log(dish);
+        const radio = `<label><input type="radio" name="orderDish" value="${dish.id}" id="${dish.id}">${dish.name}</label><br>`;
+        document.getElementById("submit").insertAdjacentHTML("beforebegin", radio)
+    });
+
+
     document.getElementById("form").addEventListener("submit", event => {
         event.preventDefault();
 
@@ -15,19 +41,39 @@ window.onload = function() {
         // // print on screen
         // const message = `The order for the customer ${name}  is the following:  ${order}. The customer may be notified by email:  ${email}`;
         // document.getElementById("container").innerHTML = message
-
+        
+        // // object
         const order = {
             name: document.getElementById("name").value,
             email: document.getElementById("email").value,
-            order: document.getElementById("order").value,
+            order: document.getElementsByName("orderDish").forEach(e => e.checked),
             printOrder(name, email, order) {
+                console.log(document.getElementsByName("orderDish"));
                 const message = `The order for the customer ${name}  is the following:  ${order}. The customer may be notified by email:  ${email}`;
                 document.getElementById("container").innerHTML = message
             }
         };
 
 
-        order.printOrder(order.name, order.email, order.order)
+        // https://www.javascripttutorial.net/javascript-dom/javascript-radio-button/
+        // https://stackoverflow.com/questions/15839169/how-to-get-value-of-selected-radio-button
+        document.getElementsByName("orderDish").forEach(e => {
+            if(e.checked) {
+                dishes.forEach(dish => {
+                    console.log(e)
+                    if(dish.id == e.value)  {
+                        order.printOrder(order.name, order.email, dish.name)
+                    }
+                })
+                
+            }
+        })
+
+
+
+        
+
+        
         
     });
 
