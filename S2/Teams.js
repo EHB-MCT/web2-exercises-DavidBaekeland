8ddc1d84f3a1a68b7a418690328a7df5fad9da8a
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-10-12 10:12:03 +0200

    Team: decribe, addPokemon

--- S2/Teams.js
+++ S2/Teams.js
@@ -4,14 +4,48 @@ class Team {
     constructor() {
         this.teamName = "default Team";
         this.trainer = "ds";
-        this.roster = ["test","qsdfqs"];
+        this.roster = [];
         console.log(this.roster)
     }
     describe() {
         console.log(...this.roster)
-        return `<h1>Team ${this.teamName} with trainer ${this.trainer} has the following pokemon: ${[...this.roster]}</h1>`
+        return `<h1 id="test">Team ${this.teamName} with trainer ${this.trainer} has the following pokemon: ${[...this.roster]}</h1>`
     }
-
+    addPokemon(pokemon) {
+        console.log(this.roster);
+        if(this.roster[0] != null) {
+            //https://www.w3schools.com/jsref/jsref_includes_array.asp
+            if (!this.roster.includes(pokemon) && this.roster.length < 6) {
+                this.roster.push(pokemon);
+                console.log(this.roster);
+                document.getElementById("test").innerHTML = this.describe();
+                alert(`The pokemon ${pokemon} has been succesfully added to the team`)
+            } else if(this.roster.length == 6) {
+                alert(`You have more than 6 pokemon`)
+            } else {
+                console.log(this.roster);
+                alert(`${pokemon} is already part of your roster`)
+            }
+            
+            // this.roster.forEach(nameRoster => {
+            //     if (pokemon != nameRoster) {
+            //         this.roster.push(pokemon);
+            //         console.log(this.roster);
+            //         document.getElementById("test").innerHTML = this.describe();
+            //     } else {
+            //         console.log(this.roster);
+            //         alert(`${pokemon} is already in team`)
+            //     }
+            // });
+        } else {
+            this.roster.push(pokemon);
+            console.log(this.roster);
+            alert(`The pokemon ${pokemon} has been succesfully added to the team`)
+            document.getElementById("test").innerHTML = this.describe();
+        }
+        
+        
+    }
 }
 
 export default Team;
