286f176e85878de7affad4415d9a50731b5cd056
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-10-23 11:25:56 +0200

    commentaar

--- technicalTest/script.js
+++ technicalTest/script.js
@@ -23,23 +23,7 @@ console.log(http)
 // var http = require('http');
 
 //create a server object:
-// https://nodejs.org/docs/v0.3.1/api/fs.html
-// https://stackoverflow.com/questions/4720343/loading-basic-html-in-node-js
-//  https://www.semicolonworld.com/question/65581/how-to-display-an-image-sent-through-http-node-js-in-the-front-end-using-fetch
-// https://nodejs.org/api/http.html
-  
-  // fetch('https://api.iconfinder.com/v4/icons/search?query=arrow&count=10&premium=false', options)
-  //   .then(response => response.json())
-  //   .then(response => {
-  //     http.createServer(function (req, res) {
-  //       resp.writeHead(200, {'Content-Type': 'text/html'})
-  //       let img = `<img src="${response.icons[0].raster_sizes[0].formats[0].preview_url}">`
-  //       // console.log()
-  //       res.write(img)
-  //       res.end(); //end the response
-  //     }).listen(5500); 
-  //   })
-  //   .catch(err => console.error(err));  
+
 
     getData('https://api.iconfinder.com/v4/icons/search?query=arrow&count=10&premium=false', options).then(response => {
       http.createServer(function (req, res) {
