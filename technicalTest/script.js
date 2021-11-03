c001c5de68917ffaf74faeb46de80870113e20dc
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-11-03 11:09:34 +0100

    js: svg ophalen en downloaden

--- technicalTest/script.js
+++ technicalTest/script.js
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 import http from "http";
+import fs from "fs";
 
 
 
@@ -27,14 +28,31 @@ const options = {
 // https://nodejs.org/api/http.html
 // https://stackoverflow.com/questions/14413559/why-is-response-write-in-node-js-not-injecting-html
 // https://www.w3schools.com/tags/tag_img.asp
+// https://www.codegrepper.com/code-examples/javascript/nodejs+send+file+http+response
+// https://nodejs.org/en/knowledge/file-system/how-to-write-files-in-nodejs/
+// https://www.w3schools.com/nodejs/nodejs_filesystem.asp
     getData('https://api.iconfinder.com/v4/icons/search?query=Stefan%20Taubert&count=4', options).then(response => {
     console.log(response.icons[3].vector_sizes[0].formats[0].download_url);  
     
     http.createServer(function (req, res) {
-        let img = `<img src="${response.icons[3].raster_sizes[0].formats[0].preview_url}" alt="Girl in a jacket">`;
-        res.writeHead(200,{"Content-Type" : "text/html"});
-        res.write(img)
-        res.end(); //end the response
+        getSvg(`${response.icons[3].vector_sizes[0].formats[0].download_url}`, options).then(response2 =>  {
+          console.log(response2)
+          
+
+          // Ophalen van svg
+          fs.writeFile('response.svg', response2, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+
+
+          // tonen van png
+          let img = `<img src="${response.icons[3].raster_sizes[0].formats[0].preview_url}" alt="Girl in a jacket">`;
+          res.writeHead(200,{"Content-Type" : "text/html"});
+          res.write(img)
+          res.end(); //end the response
+
+        });
       }).listen(5500); 
     }).catch(err => console.error(err));
 
@@ -43,3 +61,7 @@ const options = {
       return await response.json();
     }  
 
+    async function getSvg(url, options) {
+      let response = await fetch(url, options)
+      return await response.text();
+    }  
