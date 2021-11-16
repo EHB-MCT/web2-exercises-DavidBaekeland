16129a0a1364e0ac0d7330c79d6a5b5898223a3b
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-11-16 12:35:01 +0100

    S5: boardgame splitsen in meerdere documenten

--- S5/modules.js
+++ S5/modules.js
@@ -1,7 +1,24 @@
 import * as fs from 'fs/promises';
 
-// MJS -> anders await niet uitvoeren
+// Data lezen
+// MJS -> anders await niet uitvoeren of type modules package.json
 let result = await fs.readFile("boardgames.json");
 // Buffer -> json
 let data = JSON.parse(result);
-console.log(data);
+// console.log(data[120677]);
+
+// Nieuwe json
+try{
+    // niet voor arrau -> foreacht
+    // game -> kan alles zijn: qsdfsdqf werkt ook
+    for(let test in data) {
+        // console.log(test);
+        let boardgame = JSON.stringify(data[test]);
+
+        let result2 = await fs.writeFile(`${test}.json`, boardgame);
+    }
+}catch(err) {
+    console.log(err)
+}
+
+
